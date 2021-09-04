26fb5db31f3a4b848fb7ab3eac943b6a89a1d1ff
EvanCib <eciborowski@umass.edu>
2021-09-04 16:23:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+class Hw1Q1:
+
+
+    def monomial(f):
+
+            a = float(input("a = : "))
+            b = float(input("b = : "))
+            c = float(input("c = : "))
+
+            x = float(a + b)
+
+            print ( x)
+
+
+            print("You entered a: " + a)
+            print("You entered b: " + b)
+            print("You entered c: " + c)
+            #print("x equals: " + x)
+
+
+    monomial(0)
+
+
+
+    #x = int(((c//a)+(2*b//a)))
+
+
+
+
+
+
